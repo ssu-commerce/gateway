df31b7fcab8e11869179644d14a35b1f5914e793
always0ne <always0ne.dev@gmail.com>
2024-07-07 21:46:52 +0900

    lint

--- src/main/kotlin/com/ssu/commerce/gateway/config/FeignFluxConfig.kt
+++ src/main/kotlin/com/ssu/commerce/gateway/config/FeignFluxConfig.kt
@@ -1,13 +1,12 @@
 package com.ssu.commerce.gateway.config
 
-import java.util.stream.Collectors
 import org.springframework.beans.factory.ObjectProvider
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.converter.HttpMessageConverter
-
+import java.util.stream.Collectors
 
 @Configuration
 class FeignFluxConfig {
