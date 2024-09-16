ebc22e9a6722c6af85ad41d588c7600b1df6c4c2
always0ne <always0ne.dev@gmail.com>
2024-09-16 16:40:03 +0900

    fix: cors error

---
+++ src/main/kotlin/com/ssu/commerce/gateway/config/CorsConfig.kt
@@ -0,0 +1,30 @@
+package com.ssu.commerce.gateway.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties("cors")
+class CorsConfig{
+    lateinit var allowedOrigin: List<String>
+
+    @Bean
+    fun corsWebFilter(): CorsWebFilter {
+        val corsConfig = CorsConfiguration()
+            .apply {
+                allowCredentials = true
+                addAllowedHeader("*")
+                addAllowedMethod("*")
+                allowedOrigin.forEach { addAllowedHeader(it) }
+            }
+        val corsConfigurationSource = UrlBasedCorsConfigurationSource()
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfig)
+        return CorsWebFilter(corsConfigurationSource)
+    }
+}
